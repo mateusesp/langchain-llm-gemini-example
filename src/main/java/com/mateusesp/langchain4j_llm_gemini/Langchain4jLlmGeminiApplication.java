fedb344719bca80a325b0cc9e4cb4b1c0958a6f9
Mateus Esposito Ferreira <m.espositof@gmail.com>
2025-10-24 16:11:23 -0300

    initial commit

---
+++ src/main/java/com/mateusesp/langchain4j_llm_gemini/Langchain4jLlmGeminiApplication.java
@@ -0,0 +1,13 @@
+package com.mateusesp.langchain4j_llm_gemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Langchain4jLlmGeminiApplication {
+
+	void main(String[] args) {
+		SpringApplication.run(Langchain4jLlmGeminiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mateusesp/langchain4j_llm_gemini/Langchain4jLlmGeminiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateusesp.langchain4j_llm_gemini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Langchain4jLlmGeminiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
